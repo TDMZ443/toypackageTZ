416a910df6ce3dd079ec475e09f5216c01086cc8
Tristan Zook <tristanzook@gmail.com>
2026-05-14 17:10:44 +0000

    Started adding documentation

--- R/min_max_scale.R
+++ R/min_max_scale.R
@@ -1,3 +1,13 @@
+#' Scales a numeric vector using min_max scaling method
+#'
+#' @param x A numeric vector
+#'
+#' @returns A scaled numeric vector
+#' @export
+#'
+#' @examples
+#' x <- 1:5
+#' min_max_scale(x)
 min_max_scale <- function(x)
 {
   y <-(x - min(x, na.rm = TRUE))/(max(x, na.rm = TRUE)- min(x, na.rm = TRUE))
--- R/theme_academic.R
+++ R/theme_academic.R
@@ -1,3 +1,13 @@
+#' Academic theme in addition to default ggplot2 theme
+#'
+#' @param base_size A positive integer
+#'
+#' @returns A plot with the the theme added when passed with ggplot2 (centralized plot title, grid lines removed)
+#' @importFrom ggplot2 theme_classic theme element_blank element_text
+#' @export
+#'
+#' @examples
+#'
 theme_academic <- function(base_size = 12)
 {
   ggplot2::theme_classic(base_size = base_size) +
